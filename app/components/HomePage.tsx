c1375b313ec7d9d0ba30cd09e470aca1f4b80928
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-07-17 18:15:23 +0200

    C4(CHORE):fixing login logic, still takes two steps to create user object

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -4,39 +4,38 @@ import { useSession } from 'next-auth/react';
 import React, { useEffect, useState } from 'react';
 
 export default function HomePage() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const [inProcess, setInProcess] = useState(false);
-  const [currentUser, setCurrentUser] = useState<any>();
+  const [currentUser, setCurrentUser] = useState<any>({});
+  const isAuthenticated = session !== null && status === 'authenticated';
 
   const handleLogIn = async (userData: any) => {
     const userFromDb = await getUserByEmail(userData.email);
-    console.log(userFromDb);
     if (userFromDb) {
       console.log('user exists');
     } else {
       console.log('new user');
-      createNewUser(currentUser.name, currentUser.email, currentUser.image)
+      createNewUser(currentUser.name, currentUser.email, currentUser.image);
     }
   };
 
   useEffect(() => {
-    const logInFilter = async () => {
-      setCurrentUser({
-        name: session?.user?.name,
-        email: session?.user?.email,
-        image: session?.user?.image,
-      });
-
+    const loginCheck = async () => {
       if (inProcess) {
         return;
       }
       setInProcess(true);
       if (session) {
+        setCurrentUser({
+          name: session?.user?.name,
+          email: session?.user?.email,
+          image: session?.user?.image,
+        });
         await handleLogIn(currentUser);
       }
       setInProcess(false);
     };
-    logInFilter();
+    loginCheck();
   }, [session]);
 
   return (
