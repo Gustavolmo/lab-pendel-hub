81f855e62a77edc6a205b997b1a920c1c9fd06ed
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-07-18 10:08:27 +0200

    C5(FEAT): login logic and user object working properly in one step

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -1,37 +1,38 @@
 'use client';
-import { User, createNewUser, getUserByEmail } from '@/library/private/private';
+import { createNewUser, getUserByEmail } from '@/library/private/private';
+import { User } from '@/library/types/types';
 import { useSession } from 'next-auth/react';
 import React, { useEffect, useState } from 'react';
 
 export default function HomePage() {
   const { data: session, status } = useSession();
   const [inProcess, setInProcess] = useState(false);
-  const [currentUser, setCurrentUser] = useState<any>({});
-  const isAuthenticated = session !== null && status === 'authenticated';
+  // const isAuthenticated = session !== null && status === 'authenticated';
 
-  const handleLogIn = async (userData: any) => {
+  const handleLogIn = async (userData: User) => {
     const userFromDb = await getUserByEmail(userData.email);
     if (userFromDb) {
-      console.log('user exists');
+      console.log('user exists', userFromDb);
     } else {
-      console.log('new user');
-      createNewUser(currentUser.name, currentUser.email, currentUser.image);
+      console.log('new user', userFromDb);
+      createNewUser(userData.name, userData.email, userData.image);
     }
   };
 
   useEffect(() => {
+    const user = {
+      name: session?.user?.name,
+      email: session?.user?.email,
+      image: session?.user?.image,
+    }
     const loginCheck = async () => {
+      
       if (inProcess) {
         return;
       }
       setInProcess(true);
       if (session) {
-        setCurrentUser({
-          name: session?.user?.name,
-          email: session?.user?.email,
-          image: session?.user?.image,
-        });
-        await handleLogIn(currentUser);
+        await handleLogIn(user);
       }
       setInProcess(false);
     };
--- library/private/private.ts
+++ library/private/private.ts
@@ -1,32 +1,24 @@
 'use server';
-import { v4 as uuidv4 } from 'uuid';
 import { client, runMongoDb } from '../mongoConnect';
+import { User } from '../types/types';
 
-runMongoDb(); // IS THIS THE CORRECT PLACE?
-
-// const generateId = () => uuidv4();
+runMongoDb();
 const databaseName = 'pendel-hub';
 const collectionNameUsers = 'users';
 const collectionNameRoutes = 'routes';
 
-export type User = {
-  // userId: string // CONSIDER REMOVING IT
-  name: string;
-  email: string;
-  image: string;
-};
-
 // get user by email
 export const getUserByEmail = async (userEmail: string | null | undefined) => {
   const collection = client.db(databaseName).collection(collectionNameUsers);
-  return await collection.findOne({ email: userEmail });
+  const dbData = await collection.findOne({ email: userEmail });
+  return dbData
 };
 
 // post new user
 export const createNewUser = async (
-  userName: string,
-  userEmail: string,
-  userPicture: string
+  userName: string | null | undefined,
+  userEmail: string | null | undefined,
+  userPicture: string | null | undefined,
 ): Promise<any> => {
   const newUser: User = {
     name: userName,
@@ -43,24 +35,3 @@ export const createNewUser = async (
   }
 };
 
-
-// export const createUserIfNotInDb = async (userSession: any) => {
-  
-//   console.log(userSession)
-//   console.log(userSession)
-//   const sessionEmail = userObject?.email;
-  
-//   const collection = client.db(databaseName).collection(collectionNameUsers);
-//   const user = await collection.findOne({ email: sessionEmail });
-//   console.log(user);
-
-//   if (user) {
-//     console.log('USER EXISTS');
-//     return;
-
-//   } else {
-//     await collection.insertOne(userObject);
-//     console.log('NEW USER');
-//     return;
-//   }
-// };
