1f42e985d293fdfe191814cdf45d80eb74b415b1
Gustavo L.M. de Oliveira <lmo.gustavo@gmail.com>
2023-07-26 11:52:01 +0200

    C48(feat) css animation for header

--- app/components/publicComponents/HeroHeader.tsx
+++ app/components/publicComponents/HeroHeader.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../styles/HeroHeader.css';
 import Image from 'next/image';
-import banner from '../../public/banner.jpg'
+import banner from '../../public/banner.jpg';
 
 export default function HeroHeader() {
   return (
     <header className="hero-header">
-      Creat commutes, find routes & join others!
+      <p className='header-text'>Creat commutes, find routes & join others!</p>
     </header>
   );
 }
